4314bef3b2a90fa8eeb355a954b615c0c4547861
KennyFrans <franzkenny26995@gmail.com>
2025-07-08 10:50:30 +0700

    Update the yml with proper google installation

--- src/config/wdio.chrome-headless.conf.ts
+++ src/config/wdio.chrome-headless.conf.ts
@@ -1,20 +1,21 @@
 import { config as baseConfig } from './wdio.shared.conf.js';
 
 export const config: WebdriverIO.Config = {
-    ...baseConfig,
-    capabilities: [
-        {
-            browserName: "chrome",
-            'goog:chromeOptions': {
-                args: [
-                    '--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML. like Gecko) Chrome/94.0.4606.61 Safari/537.36',
-                    '--disable-blink-features=AutomationControlled',
-                    '--headless',
-                    '--no-sandbox',
-                    '--disable-gpu',
-                    '--start-maximized',
-                ]
-            }
-        }
-    ]
+  ...baseConfig,
+  capabilities: [
+    {
+      browserName: 'chrome',
+      'goog:chromeOptions': {
+        args: [
+          '--headless=new', // modern headless mode (better support)
+          '--no-sandbox',
+          '--disable-gpu',
+          '--disable-dev-shm-usage',
+          '--disable-blink-features=AutomationControlled',
+          '--start-maximized',
+          '--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36'
+        ]
+      }
+    }
+  ]
 };
